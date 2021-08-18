0ec4de1dcf6b7a5171144d0b3ef129cb1ee0a1a2
Juan Hernandez <yoojuaan@gmail.com>
2021-08-18 14:22:37 -0300

    Se agrego CORS para el navegador

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const app = express()
 const port = 5000
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 app.listen(port, () => {
   console.log('Server running on port ' + port + '.')
 })
 
-app.get('/ambient-temperature/', api.getAmbientTemperature)
+app.post('/ambient-temperature/', api.getAmbientTemperature)
 app.get('/ambient-humidity/', api.getAmbientHumidity)
 app.get('/soil-humidity/', api.getSoilHumidity)
 app.get('/lux/', api.getLux)
