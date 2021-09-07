559a51645ea2efe7d4fcc0c734b2628221616c44
Juampi <jphernandez107@gmail.com>
2021-09-06 21:16:47 -0300

    Se devuelven las ubicaciones como un json anidado con los invernaderos, secciones y sectores que corresponden. Tambien se devuelve la lista ordenada alfabeticamente

--- api.js
+++ api.js
@@ -98,7 +98,31 @@ const getLocationDetails = async (req, res) => {
 
     pool.query(sqlQuery)
     .then(results => {
-        res.status(200).json(results.rows)
+        var greenhouses = []
+        for(var loc of results.rows) { // Armamos el array de objetos anidados
+            var gh = greenhouses.find(gh => gh.greenhouse == loc.greenhouse)
+            if (gh != undefined) {
+                var section = gh.sections.find(sect => sect.section == loc.section)
+                if (section != undefined) {
+                    var sector = section.sectors.find(sect => sect.sector == loc.sector)
+                    if (sector == undefined) {
+                        section.sectors.push({sector: loc.sector})
+                    } 
+                } else {
+                    var sectors = []
+                    sectors.push({sector: loc.sector})
+                    gh.sections.push({section: loc.section, sectors: sectors})
+                }
+            } else {
+                var sectors = []
+                sectors.push({sector: loc.sector})
+                var sections = []
+                sections.push({section: loc.section, sectors:sectors})
+                greenhouses.push({greenhouse: loc.greenhouse, sections:sections})
+            }
+        }
+        greenhouses.sort(orderGreenhouses)
+        res.status(200).json(greenhouses)
     })
     .catch(error => {
         console.log(error)
@@ -110,6 +134,7 @@ const getGreenhouses = async (req, res) => {
     const sqlQuery = "SELECT DISTINCT greenhouse from " + table;
     pool.query(sqlQuery)
     .then(results => {
+        // results.rows.sort(orderGreenhouses)
         res.status(200).json(results.rows)
     })
     .catch(error => {
@@ -119,13 +144,13 @@ const getGreenhouses = async (req, res) => {
 
 const getRealTimeData = async (req, res) => {
     const filterParams = getSQLFilters(req)
-    let room_temp = "(select 'room_temperature' as sensor, ROUND(AVG(value), 2) as value, to_timestamp(floor((extract('epoch' from created_date) / 60 )) * 60) as date from temperatura_ambiente " + filterParams + " group by date order by date DESC limit 1)"
-    let room_hum = "(select 'room_humidity' as sensor, ROUND(AVG(value), 2) as value, to_timestamp(floor((extract('epoch' from created_date) / 60 )) * 60) as date from humedad_ambiente " + filterParams + " group by date order by date DESC limit 1)"
-    let soil_hum = "(select 'soil_humidity' as sensor, ROUND(AVG(value), 2) as value, to_timestamp(floor((extract('epoch' from created_date) / 60 )) * 60) as date from humedad_suelo " + filterParams + " group by date order by date DESC limit 1)"
-    let lux = "(select 'lux' as sensor, ROUND(AVG(value), 2) as value, to_timestamp(floor((extract('epoch' from created_date) / 60 )) * 60) as date from luz " + filterParams + " group by date order by date DESC limit 1);"
+    let room_temp = "(SELECT 'room_temperature' AS sensor, ROUND(AVG(value), 2) AS VALUE, location, greenhouse, section, sector, to_timestamp(floor((extract('epoch' from created_date) / 60 )) * 60) AS date FROM temperatura_ambiente AS sensorTable INNER JOIN locations ON locations.id = sensorTable.location " + filterParams + " GROUP BY date, location, greenhouse, section, sector ORDER BY date DESC LIMIT 1)"
+    let room_hum = "(SELECT 'room_humidity' AS sensor, ROUND(AVG(value), 2) AS VALUE, location, greenhouse, section, sector, to_timestamp(floor((extract('epoch' from created_date) / 60 )) * 60) AS date FROM humedad_ambiente AS sensorTable INNER JOIN locations ON locations.id = sensorTable.location " + filterParams + " GROUP BY date, location, greenhouse, section, sector ORDER BY date DESC LIMIT 1)"
+    let soil_hum = "(SELECT 'soil_humidity' AS sensor, ROUND(AVG(value), 2) AS VALUE, location, greenhouse, section, sector, to_timestamp(floor((extract('epoch' from created_date) / 60 )) * 60) AS date FROM humedad_suelo AS sensorTable INNER JOIN locations ON locations.id = sensorTable.location " + filterParams + " GROUP BY date, location, greenhouse, section, sector ORDER BY date DESC LIMIT 1)"
+    let lux = "(SELECT 'lux' AS sensor, ROUND(AVG(value), 2) AS VALUE, location, greenhouse, section, sector, to_timestamp(floor((extract('epoch' from created_date) / 60 )) * 60) AS date FROM luz AS sensorTable INNER JOIN locations ON locations.id = sensorTable.location " + filterParams + " GROUP BY date, location, greenhouse, section, sector ORDER BY date DESC LIMIT 1);"
 
     var sqlQuery = room_temp + " union " + room_hum + " union " + soil_hum + " union " + lux;
-    // console.log(sqlQuery)
+    console.log(sqlQuery)
 
     pool.query(sqlQuery)
     .then(results => {
@@ -155,6 +180,7 @@ function getSQLFilters(req) {
     console.log(req.query)
 
     if (req.query.locationIds != undefined && req.query.locationIds.length > 0) { where.push(getLocationFilter(req.query.locationIds, "location"))}
+    if (req.query.greenhouses != undefined && req.query.greenhouses.length > 0) { where.push(getLocationQuery(req, "locations."))}
     if (req.query.fromDate != undefined && req.query.fromDate != "") { where.push(" created_date >= '" + req.query.fromDate + "'")}
     if (req.query.toDate != undefined && req.query.toDate != "") { where.push(" created_date <= '" + req.query.toDate + "'")}
     if (req.query.fromValue != undefined) { where.push(" value >= " + req.query.fromValue)}
@@ -177,6 +203,31 @@ function getLocationFilter(locationIdArray, filterParam = "location") {
     if(locations.length > 0) {
         locationSQL = " (" + locations.join(" OR ") + ") "
     }
+    return locationSQL
+}
+
+function getLocationQuery(req, filterParam = "locations.") {
+    var greenhouses = []
+    var sections = []
+    var sectors = []
+    if (req.query.greenhouses != undefined)
+        for (let greenhouse of req.query.greenhouses) 
+            greenhouses.push(filterParam + "greenhouse = '" + greenhouse + "'")
+    if (req.query.sections != undefined)
+        for (let section of req.query.sections)
+            sections.push(filterParam + "section = '" + section + "'")
+    if (req.query.sectors != undefined)
+        for (let sector of req.query.sectors)
+            sectors.push(filterParam + "sector = '" + sector + "'")
+
+    var locations = []
+    if (greenhouses.length > 0) locations.push(" (" + greenhouses.join(" OR ") + ") ")
+    if (sections.length > 0) locations.push(" (" + sections.join(" OR ") + ") ")
+    if (sectors.length > 0) locations.push(" (" + sectors.join(" OR ") + ") ")
+    
+    var locationSQL = ""
+    if (locations.length > 0) locationSQL = " (" + locations.join(" AND ") + ") "
+
     return locationSQL
 }
 
@@ -214,4 +265,39 @@ module.exports = {
     getLocationDetails,
     getGreenhouses,
     getRealTimeData
+}
+
+
+function orderSectors(sectorA, sectorB) {
+    if (sectorA.sector > sectorB.sector) {
+        return 1;
+    }
+    if (sectorA.sector < sectorB.sector) {
+        return -1;
+    }
+    return 0;
+}
+
+function orderSections(sectionA, sectionB) {
+    sectionA.sectors.sort(orderSectors)
+    sectionB.sectors.sort(orderSectors)
+    if (sectionA.section > sectionB.section) {
+        return 1;
+    }
+    if (sectionA.section < sectionB.section) {
+        return -1;
+    }
+    return 0;
+}
+
+function orderGreenhouses(ghA, ghB) {
+    ghA.sections.sort(orderSections)
+    ghB.sections.sort(orderSections)
+    if (ghA.greenhouse > ghB.greenhouse) {
+        return 1;
+    }
+    if (ghA.greenhouse < ghB.greenhouse) {
+        return -1;
+    }
+    return 0;
 }
