84d8f8b97d67fa26f7fa0950fb703cab46a6eac2
Juampi <jphernandez107@gmail.com>
2021-09-09 20:48:30 -0300

    Cambios en el puerto para heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const api = require('./api')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use((req, res, next) => {
